48ca6e7efa161125df4716ee727f1a6a44514baf
João Vitor Medeiros <joaoolimedeiros@gmail.com>
2023-11-02 14:07:40 -0300

    refactor: :recycle: primeiros testes de projeto

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,22 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
+Route.get('/docs/', async ({ view }) => {
+  return 1
+})
 Route.get('/', async ({ view }) => {
-  return view.render('welcome')
+  return view.render('errors/not-found')
+})
+Route.get('/solange/', async ({ view }) => {
+  const data = {}
+  data.alunos = ['João', 'Tati']
+  data.materias = ['Ciencias', 'Matematica']
+  return view.render('index', { users: data.alunos })
+})
+Route.get('/teste/', async ({ view }) => {
+  const data = {}
+  data.alunos = ['João', 'Tati']
+  data.materias = ['Ciencias', 'Matematica']
+
+  return data
 })
